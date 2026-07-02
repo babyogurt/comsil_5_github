b5c716f743535aba47e4a5823c861892f75b6d09
babyogurt <choieugene5153@gmail.com>
2026-07-02 11:31:27 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int w=13;
+    double r=0.165;
+
+    cout<<fixed;
+
+    cout.precision(6);
+
+    cout<<w<<" * "<<r<<" = "<<w*r;
+    return 0;
+}

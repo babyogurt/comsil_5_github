bbe197e9956454b8ebf965b077ae02cd55277b55
babyogurt <choieugene5153@gmail.com>
2026-07-03 18:24:51 +0900

    [Passed] 특정 위치의 수 (2ms, 0MB)

---
+++ trail1/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a[10];
+    int tot=0;
+    for(int i=0;i<10;i++){
+        cin>>a[i];
+        
+    }
+    tot=a[2]+a[4]+a[9];
+    cout<< tot;
+
+    return 0;
+}

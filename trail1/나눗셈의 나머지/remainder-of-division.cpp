65c66f86893e139ef6606904cadefccf9547a056
babyogurt <choieugene5153@gmail.com>
2026-07-04 10:52:42 +0900

    [Passed] 나눗셈의 나머지 (2ms, 0MB)

---
+++ trail1/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,B;
+    cin>>A>>B;
+    int arr[1001]={};
+    int n=A/B;
+
+    while(A>1){
+        
+        
+        arr[A%B]++;
+        A=A/B;
+        
+    }
+    int sum=0;
+    for(int i=0;i<n+1;i++){
+        //cout<<arr[i]<<" ";
+        sum+=arr[i]*arr[i];
+    }
+    cout<<sum;
+    return 0;
+}

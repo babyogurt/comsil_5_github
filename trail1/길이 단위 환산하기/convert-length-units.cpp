abda2636e77f2669892c1be4acb0bae15516e3c0
babyogurt <choieugene5153@gmail.com>
2026-07-09 12:49:29 +0900

    [Passed] 길이 단위 환산하기 (2ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+
+    cout<<fixed;
+    cout.precision(1);
+    cin>>n;
+    cout<<n*30.48;
+    return 0;
+}

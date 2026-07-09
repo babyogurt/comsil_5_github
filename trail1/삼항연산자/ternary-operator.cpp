2e1688e3779a16e55f787da6cb7c72288ea3e986
babyogurt <choieugene5153@gmail.com>
2026-07-09 16:01:48 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

---
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    if(n==100){
+        cout<<"pass";
+    }
+    else cout<<"failure";
+    return 0;
+}

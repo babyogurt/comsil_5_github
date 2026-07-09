ca062ac195d011ee8d5a99c8fe47bf3820397d43
babyogurt <choieugene5153@gmail.com>
2026-07-09 12:31:11 +0900

    [Passed] 길이 단위 변환하기 (2ms, 0MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a=9.2,b=1.3;
+
+    cout<<fixed;
+    cout.precision(1);
+    
+
+    cout<<a<<"ft = "<<a*30.48<<"cm"<<endl;
+    cout<<b<<"mi = "<<b*160934<<"cm";
+    return 0;
+}

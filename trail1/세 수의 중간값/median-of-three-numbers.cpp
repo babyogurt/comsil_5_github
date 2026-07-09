e2dd9aa86f7d670c4d31881e1d27cbd402c8340f
babyogurt <choieugene5153@gmail.com>
2026-07-09 18:39:48 +0900

    [Passed] 세 수의 중간값 (2ms, 0MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(b>a && b<c) cout<<true;
+
+    else cout<<false;
+    return 0;
+}

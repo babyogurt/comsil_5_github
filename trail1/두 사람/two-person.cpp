e203162cf924a71ca640047e00ed4b4aedbf7903
babyogurt <choieugene5153@gmail.com>
2026-07-10 10:41:07 +0900

    [Passed] 두 사람 (2ms, 0MB)

---
+++ trail1/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a1,b1;
+    char a2,b2;
+
+    cin>>a1>>a2>>b1>>b2;
+
+    if((a1>=19 && a2=='M')||(b1>=19 && b2=='M')){
+        cout<<"1";
+    }
+    else cout<<"0";
+    return 0;
+}

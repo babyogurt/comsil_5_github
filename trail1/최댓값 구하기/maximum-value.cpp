d6ea9ef580e936437eb67cdc5659dd76d3e49aae
babyogurt <choieugene5153@gmail.com>
2026-07-10 10:54:01 +0900

    [Passed] 최댓값 구하기 (2ms, 0MB)

---
+++ trail1/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a<=b && c<=b){
+        cout<<b;
+    }
+    else if(b<=a && c<=a){
+        cout<<a;
+    }
+    else cout<<c;
+    
+    return 0;
+}

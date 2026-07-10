236c2ca443a2ab728253e2a18be4a7a7fcbb6cda
babyogurt <choieugene5153@gmail.com>
2026-07-10 11:34:20 +0900

    [Passed] 짝수만 출력 2 (2ms, 0MB)

---
+++ trail1/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>b>>a;
+    int i=b;
+    
+
+    while(i>=a){
+        if(i%2==0){
+            cout<<i<<" ";
+            
+        }
+        i--;
+    }
+    return 0;
+}

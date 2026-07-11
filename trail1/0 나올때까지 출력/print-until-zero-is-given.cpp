898605d0cfb745e3ee58dc570ae8d2cf04471ee6
babyogurt <choieugene5153@gmail.com>
2026-07-11 11:47:05 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    
+        
+    while(a!=0){
+        
+        cout<<a<<endl;
+        cin>>a;
+    }
+    return 0;
+}

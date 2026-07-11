1bedf40bbc3fab248f21d96f79b5eec8c9057cce
babyogurt <choieugene5153@gmail.com>
2026-07-11 10:53:15 +0900

    [Passed] 등장횟수 확인 (2ms, 0MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int cnt=0;
+    int a;
+
+    for(int i=0;i<5;i++){
+        cin>>a;
+        if(a%2==0){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}

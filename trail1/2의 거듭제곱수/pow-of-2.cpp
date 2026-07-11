f14597169de73076b12b39a9c4f645039b800a0d
babyogurt <choieugene5153@gmail.com>
2026-07-11 19:08:54 +0900

    [Passed] 2의 거듭제곱수 (2ms, 0MB)

---
+++ trail1/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    int cnt=0;
+
+    while(n!=1){
+        n/=2;
+        cnt++;
+
+    }
+    cout<<cnt;
+    return 0;
+}

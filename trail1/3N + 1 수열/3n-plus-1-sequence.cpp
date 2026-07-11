40e3d1bdcabc070ac54ceb9e7d003e0f5e5f243b
babyogurt <choieugene5153@gmail.com>
2026-07-11 19:00:01 +0900

    [Passed] 3N + 1 수열 (2ms, 0MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    int cnt=0;
+
+    while(n!=1){
+        if(n%2==0){
+            n=n/2;
+        }
+        else n=3*n+1;
+        cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}

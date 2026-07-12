c9aab172be92c93c6f3c96f9481432ee9d1adef6
babyogurt <choieugene5153@gmail.com>
2026-07-12 20:36:25 +0900

    [Passed] 공약수의 유무 (2ms, 0MB)

---
+++ trail1/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+
+    bool check=false;
+
+    for(int i=a;i<=b;i++){
+        if(1920%i==0 && 2880%i==0){
+            check=true;
+        }
+        
+    }
+    cout<<check;
+    return 0;
+}

d660969c751dec146a734f2e9923d20d9d62bc6f
babyogurt <choieugene5153@gmail.com>
2026-07-12 21:21:39 +0900

    [Passed] 별표 출력하기 3 (1ms, 0MB)

---
+++ trail1/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    for(int i=n;i>0;i--){
+        for(int p=0;p<2*(n-i);p++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i-1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}

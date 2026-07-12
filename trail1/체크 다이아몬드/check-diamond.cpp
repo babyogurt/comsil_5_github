3181dfbf62a8a1ea60c1a2dfd81b434c3fa1f510
babyogurt <choieugene5153@gmail.com>
2026-07-12 21:53:38 +0900

    [Passed] 체크 다이아몬드 (1ms, 0MB)

---
+++ trail1/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int p=0;p<n-1-i;p++){
+            cout<<" ";
+        }
+        for(int j=0;j<i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=1;i<n;i++){
+        for(int p=0;p<i;p++){
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

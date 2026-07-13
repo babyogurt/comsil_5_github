ac7360fb3c992fa19e9094c471db1596360347da
babyogurt <choieugene5153@gmail.com>
2026-07-13 14:42:37 +0900

    [Passed] 완전수 (2ms, 0MB)

---
+++ trail1/완전수/perfect-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+
+    int cnt=0;
+    int sum=0;
+
+    for(int i=a;i<=b;i++){
+        sum=0;
+        for(int j=1;j<i;j++){
+            if(i%j==0){
+                sum+=j;
+            }
+            
+        }if(sum==i) cnt++;
+    }
+    cout<<cnt<<endl;
+    return 0;
+}

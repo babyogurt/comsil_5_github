f7b9a8266cdff065567312a4d2b01cc1cf1e571a
babyogurt <choieugene5153@gmail.com>
2026-07-13 14:49:08 +0900

    [Passed] 소수 구하기 (2ms, 0MB)

---
+++ trail1/소수 구하기/get-prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    int cnt=0;
+
+    for(int i=2;i<=n;i++){
+        cnt=0;
+        for(int j=2;j<i;j++){
+            if(i%j==0) cnt++;
+        }
+        if(cnt==0) cout<<i<<" ";
+    }
+    return 0;
+}

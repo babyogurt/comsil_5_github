e72799dc6531b1c407653035545d2304767a5e11
babyogurt <choieugene5153@gmail.com>
2026-07-15 00:17:21 +0900

    [Passed] 문자열에서 문자 출력 (2ms, 0MB)

---
+++ trail1/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s;
+    cin>>s;
+
+    for(int i=0;i<s.length();i++){
+        cout<<s[i]<<endl;
+    }
+    return 0;
+}

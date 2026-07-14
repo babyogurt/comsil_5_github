945a24a6d83e5c23e26ca8bebfba093b259963a9
babyogurt <choieugene5153@gmail.com>
2026-07-14 23:24:06 +0900

    [Passed] 문자열 두번 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s;
+
+    cin>>s;
+
+    cout<<s<<endl<<s;
+
+    return 0;
+}

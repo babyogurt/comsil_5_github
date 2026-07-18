c398fc93005ebd6fe80b6c6cc0cc0abb7aa7a44f
babyogurt <choieugene5153@gmail.com>
2026-07-18 10:57:18 +0900

    [Passed] 두 문자열을 이어붙였을 때 (2ms, 0MB)

---
+++ trail1/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a,b;
+    cin>>a>>b;
+    if(a+b==b+a) cout<<"true";
+    else cout<<"false";
+    return 0;
+}

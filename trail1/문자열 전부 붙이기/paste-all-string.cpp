a027737a816dea5ac043dcedbfcf1bb0a3a83fb0
babyogurt <choieugene5153@gmail.com>
2026-07-18 10:45:05 +0900

    [Passed] 문자열 전부 붙이기 (2ms, 0MB)

---
+++ trail1/문자열 전부 붙이기/paste-all-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int  n;
+    string s;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>s;
+        cout<<s;
+    }
+
+    return 0;
+}

2697a98d68dd09d71536187b401a1936563dcd4a
babyogurt <choieugene5153@gmail.com>
2026-07-20 12:17:38 +0900

    [Passed] a로 채워넣기 (2ms, 0MB)

---
+++ trail1/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s;
+    cin>>s;
+
+    s[1]='a';
+    s[s.length()-2]='a';
+
+    cout<<s;
+    return 0;
+}

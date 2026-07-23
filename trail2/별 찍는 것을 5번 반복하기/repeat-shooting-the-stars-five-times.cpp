dcbfdc5e8d70881561e4d7ec8afd7b44e3a5160a
babyogurt <choieugene5153@gmail.com>
2026-07-23 11:27:00 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+    
+    
+    void Star(){
+        for(int i=0;i<10;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+int main() {
+    // Please write your code here.
+    
+
+
+    for(int i=0;i<5;i++){
+        Star();
+    }
+    return 0;
+}

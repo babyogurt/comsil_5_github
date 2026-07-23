704f7b9e2149b5e75008b9838863de0ab5f543e0
babyogurt <choieugene5153@gmail.com>
2026-07-23 12:39:43 +0900

    [Passed] 함수를 이용한 소수 판별 (2ms, 0MB)

---
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int Prime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+
+    int sum=0;
+    
+    for(int i=a;i<=b;i++){
+        if(Prime(i)) sum+=i;
+
+    }
+
+    cout<<sum;
+
+   
+
+    return 0;
+}

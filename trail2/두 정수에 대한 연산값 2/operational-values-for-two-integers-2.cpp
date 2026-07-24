9262ce981e1b634d4debdc4b571e67cd2e9bc2f8
babyogurt <choieugene5153@gmail.com>
2026-07-24 12:53:26 +0900

    [Passed] 두 정수에 대한 연산값 2 (2ms, 0MB)

---
+++ trail2/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Result(int &a,int &b){
+    if(a>b){
+        a*=2;
+        b+=10;
+    }
+    else {
+        a+=10;
+        b*=2;
+    }
+}
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+
+    Result(a,b);
+
+    cout<<a<<" "<<b;
+    return 0;
+}

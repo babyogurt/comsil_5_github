6dc63f65deb9413cb4197335aa28f6544698bbae
babyogurt <choieugene5153@gmail.com>
2026-07-27 14:11:44 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (2ms, 0MB)

---
+++ trail2/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int f(int n){
+
+    if(n==1) return 0;
+
+    if(n%2==0) return f(n/=2)+1;
+    else return f(3*n+1)+1;
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    cout<<f(n);
+    return 0;
+}

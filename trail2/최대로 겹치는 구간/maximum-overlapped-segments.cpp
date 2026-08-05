497eddb922a1b9e884860dac334d44895d4539cd
babyogurt <choieugene5153@gmail.com>
2026-08-05 18:34:22 +0900

    [Passed] 최대로 겹치는 구간 (2ms, 0MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    int arr[201]={};
+
+    int a,b;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        for(int j=a+100;j<b+100;j++){
+            arr[j]++;
+        }
+    }
+
+    int max=0;
+
+    for(int i=0;i<200;i++){
+        if(arr[max]<arr[i]){
+            max=i;
+        }
+    }
+
+    cout<<arr[max];
+    return 0;
+}

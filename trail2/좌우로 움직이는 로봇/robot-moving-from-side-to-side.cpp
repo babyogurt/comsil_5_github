0c1e1112d8965eaf982cff340ed20c9ec2c23691
babyogurt <choieugene5153@gmail.com>
2026-08-07 13:01:10 +0900

    [Passed] 좌우로 움직이는 로봇 (20ms, 16MB)

--- trail2/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ trail2/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,13 +8,7 @@ char d[50000];
 int t_b[50000];
 char d_b[50000];
 
-int A[2000005] = {};
-int B[2000005] = {};
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
@@ -26,45 +19,80 @@ int main() {
         cin >> t_b[i] >> d_b[i];
     }
 
-    int timea = 1;
-    for(int i = 0; i < n; i++){
-        int temp_t = t[i]; // 원본 시간 보존
-        while(temp_t--){
-            if(d[i] == 'R') A[timea] = A[timea - 1] + 1;
-            else if(d[i] == 'L') A[timea] = A[timea - 1] - 1;
-            timea++;
+
+    int A[2000001]={};
+    int timea=1;
+
+    int B[2000001]={};
+    int timeb=1;
+
+
+    for(int i=0;i<n;i++){
+        int T=t[i];
+        if(d[i]=='R'){
+            while(T--){
+                A[timea]=A[timea-1]+1;
+                timea++;
+
+            }
+        }
+        else if(d[i]=='L'){
+            while(T--){
+                A[timea]=A[timea-1]-1;
+                timea++;
+
+            }
         }
     }
 
-    int timeb = 1;
-    for(int i = 0; i < m; i++){
-        int temp_t = t_b[i]; // 원본 시간 보존
-        while(temp_t--){
-            if(d_b[i] == 'R') B[timeb] = B[timeb - 1] + 1;
-            else if(d_b[i] == 'L') B[timeb] = B[timeb - 1] - 1;
-            timeb++;
+    for(int i=0;i<m;i++){
+        int T=t_b[i];
+        if(d_b[i]=='R'){
+            while(T--){
+                B[timeb]=B[timeb-1]+1;
+                timeb++;
+
+            }
+        }
+        else if(d_b[i]=='L'){
+            while(T--){
+                B[timeb]=B[timeb-1]-1;
+                timeb++;
+
+            }
         }
     }
 
+    // timea와 timeb 중 더 오래 걸린 시간을 구합니다.
     int max_time = max(timea, timeb);
 
-    // 남은 시간 동안 마지막 위치 유지
+    // 만약 B가 먼저 끝났다면(timeb < max_time), B의 남은 시간 동안 마지막 위치를 유지시킵니다.
     for (int i = timeb; i < max_time; i++) {
         B[i] = B[timeb - 1];
     }
+
+    // 만약 A가 먼저 끝났다면(timea < max_time), A의 남은 시간 동안 마지막 위치를 유지시킵니다.
     for (int i = timea; i < max_time; i++) {
         A[i] = A[timea - 1];
     }
 
-    int meet = 0;
-    for(int i = 1; i < max_time; i++){
-        // 현재 위치가 같고, 직전 위치가 달랐을 때
-        if(A[i] == B[i] && A[i - 1] != B[i - 1]){
-            meet++;
-        }
-    }
+    
 
-    cout << meet;
+    int meet=0;
+
+    
+        for(int i=1;i<max_time;i++){
+            if(A[i]==B[i] && A[i-1]!=B[i-1]){
+                meet++;
+            }
+        }
+    
+    
+
+    cout<<meet;
+    
+
+    // Please write your code here.
 
     return 0;
 }

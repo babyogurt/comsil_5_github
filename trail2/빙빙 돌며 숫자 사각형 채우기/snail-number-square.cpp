fe56b2e23c3bc35b02aef3b95abbdad249475416
babyogurt <choieugene5153@gmail.com>
2026-08-08 10:07:41 +0900

    [Passed] 빙빙 돌며 숫자 사각형 채우기 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+bool InRange(int x, int y,int n, int m) {
+    return (0 <= x && x < n && 0 <= y && y < m);
+}
+
+
+int main() {
+    // Please write your code here.
+    int dr[4]={0,1,0,-1};
+    int dc[4]={1,0,-1,0};
+
+    int n,m;
+    cin>>n>>m;
+
+    int arr[100][100]={};
+
+    int r=0,c=0;
+    int dir=0;
+    int nr=r+dr[dir],nc=c+dc[dir];
+    arr[r][c]=1;
+
+    int k=1;
+
+    for(int k=2;k<=n*m;k++){
+        nr=r+dr[dir];
+        nc=c+dc[dir];
+        if(arr[nr][nc]!=0 || !InRange(nr,nc,n,m)){
+            dir=(dir+1)%4;
+            nr=r+dr[dir];
+            nc=c+dc[dir];
+
+        }
+        
+            r=nr;
+            c=nc;
+            arr[r][c]=k;    
+        
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

7a24c6fe9463f43077fa21d0ddeb68ec2f22b4e4
babyogurt <choieugene5153@gmail.com>
2026-08-09 15:15:18 +0900

    [Passed] 빙빙 돌며 사각형 채우기 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+bool InRange(int x, int y,int n, int m) {
+    return (0 <= x && x < n && 0 <= y && y < m);
+}
+
+
+int main() {
+    // Please write your code here.
+    char alpa[28]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    int dr[4]={0,1,0,-1};
+    int dc[4]={1,0,-1,0};
+
+    int n,m;
+    cin>>n>>m;
+
+    char arr[100][100]={};
+
+    int r=0,c=0;
+    int dir=0;
+    int nr=r+dr[dir],nc=c+dc[dir];
+    arr[r][c]=alpa[0];
+
+
+    for(int k=1;k<n*m;k++){
+        nr=r+dr[dir];
+        nc=c+dc[dir];
+        if(!InRange(nr,nc,n,m) || arr[nr][nc]!='\0'){
+            dir=(dir+1)%4;
+
+        }
+        
+            r+=dr[dir];
+            c+=dc[dir];
+            
+            arr[r][c]=alpa[k%26];    
+        
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

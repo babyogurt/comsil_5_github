4b2230247915c1c5117db4f2ac059b5693755990
babyogurt <choieugene5153@gmail.com>
2026-09-15 17:08:20 +0900

    [Passed] 입력받아 계산 (127ms, 10MB)

---
+++ trail1/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scin = new Scanner(System.in);
+        int a=scin.nextInt();
+        a+=2;
+        System.out.print(a);
+    }
+}

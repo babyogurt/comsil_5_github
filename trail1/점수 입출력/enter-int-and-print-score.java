8285b28a380765219856581785862dbe84faaddf
babyogurt <choieugene5153@gmail.com>
2026-09-15 17:11:18 +0900

    [Passed] 점수 입출력 (134ms, 10MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scin = new Scanner(System.in);
+        int N=scin.nextInt();
+        System.out.printf("Your score is %d point.",N);
+
+    }
+}

fdc9347eaeb8c7ede2cfc38636785cb522045e3b
babyogurt <choieugene5153@gmail.com>
2026-09-15 16:55:20 +0900

    [Passed] 변수 값 교체하기 3 (97ms, 9MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=3,b=5;
+        int tmp=a;
+        a=b;
+        b=tmp;
+
+        System.out.print(a+"\n"+b);
+    }
+}

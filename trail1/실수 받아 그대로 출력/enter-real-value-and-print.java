3cdfd0ebe6f28e21252a58aadaca16675cad8fab
babyogurt <choieugene5153@gmail.com>
2026-09-15 17:18:27 +0900

    [Passed] 실수 받아 그대로 출력 (152ms, 12MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scin = new Scanner(System.in);
+        double N= scin.nextDouble();
+        System.out.printf("%.2f",N);
+    }
+}

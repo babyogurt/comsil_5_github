df629587e05a44eb5c802859695756a46755d171
babyogurt <choieugene5153@gmail.com>
2026-09-22 15:06:51 +0900

    [Passed] 청소당번 정하기 (123ms, 10MB)

---
+++ trail1/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scin=new Scanner(System.in);
+        int n=scin.nextInt();
+
+        int a=0,b=0,c=0;
+
+        for(int i=1;i<=n;i++){
+            if(i%12==0) c++;
+            else if(i%3==0) b++;
+            else if(i%2==0) a++;
+        }
+
+        System.out.print(a+" "+b+" "+c);
+    }
+}

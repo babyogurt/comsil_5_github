818e2ae6d89436f185d3eb9b3665e8d4a484374d
babyogurt <choieugene5153@gmail.com>
2026-09-22 15:17:57 +0900

    [Passed] 홀수만 더하기 (128ms, 10MB)

---
+++ trail1/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scin=new Scanner(System.in);
+
+        int n=scin.nextInt();
+
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            int a=scin.nextInt();
+            if(a%2==1 && a%3==0){
+                cnt+=a;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
